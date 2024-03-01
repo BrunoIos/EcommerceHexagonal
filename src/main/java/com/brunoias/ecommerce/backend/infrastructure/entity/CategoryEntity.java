715fb985dcaeecc19fbddcbba39b46a57cc2cf1d
Bruno Ias <brunofelicianobritoias@gmail.com>
2024-03-01 20:30:32 -0300

    Category Entity

---
+++ src/main/java/com/brunoias/ecommerce/backend/infrastructure/entity/CategoryEntity.java
@@ -0,0 +1,23 @@
+package com.brunoias.ecommerce.backend.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateUpdated;
+
+
+}
+
