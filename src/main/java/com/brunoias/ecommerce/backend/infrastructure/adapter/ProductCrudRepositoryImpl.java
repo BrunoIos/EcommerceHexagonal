219c09ac786c879a3eb5a107972db11263186489
Bruno Ias <brunofelicianobritoias@gmail.com>
2024-03-02 21:55:29 -0300

    Product Crud Implemented

--- src/main/java/com/brunoias/ecommerce/backend/infrastructure/adapter/ProductCrudRepositoryImpl.java
+++ src/main/java/com/brunoias/ecommerce/backend/infrastructure/adapter/ProductCrudRepositoryImpl.java
@@ -2,25 +2,36 @@ package com.brunoias.ecommerce.backend.infrastructure.adapter;
 
 import com.brunoias.ecommerce.backend.domain.model.Product;
 import com.brunoias.ecommerce.backend.domain.port.IProductRepository;
+import com.brunoias.ecommerce.backend.infrastructure.mapper.ProductMapper;
 
 public class ProductCrudRepositoryImpl implements IProductRepository {
+    private final IProductCrudRepository iProductCrudRepository;
+
+    private final ProductMapper productMapper;
+
+    public ProductCrudRepositoryImpl(IProductCrudRepository iProductCrudRepository, ProductMapper productMapper) {
+        this.iProductCrudRepository = iProductCrudRepository;
+        this.productMapper = productMapper;
+    }
+
+
     @Override
     public Product save(Product product) {
-        return null;
+        return productMapper.toProduct(productMapper.toProductEntity(product));
     }
 
     @Override
     public Iterable<Product> findAll() {
-        return null;
+        return productMapper.toProducts(iProductCrudRepository.findAll());
     }
 
     @Override
     public Product findById(Integer id) {
-        return null;
+        return productMapper.toProduct(iProductCrudRepository.findById(id).get());
     }
 
     @Override
     public void deleteByID(Integer id) {
-
+        iProductCrudRepository.deleteById(id);
     }
 }
