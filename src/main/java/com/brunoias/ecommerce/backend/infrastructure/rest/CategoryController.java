74005bcd06d17ddf1429f3b05507ab4733da08b4
Bruno Ias <brunofelicianobritoias@gmail.com>
2024-03-03 20:58:00 -0300

    Using ResponseEntity ins Controllers

--- src/main/java/com/brunoias/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/brunoias/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -3,6 +3,9 @@ package com.brunoias.ecommerce.backend.infrastructure.rest;
 import com.brunoias.ecommerce.backend.application.CategoryService;
 import com.brunoias.ecommerce.backend.domain.model.Category;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,22 +20,23 @@ public class CategoryController {
     }
 
     @PostMapping
-    public Category save(@RequestBody Category category) {
-        return categoryService.save(category);
+    public ResponseEntity<Category> save(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public Iterable<Category> findAll(){
-        return categoryService.findAll();
+    public ResponseEntity<Iterable<Category>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
     }
 
     @GetMapping("/{id}")
-    public Category findById(@PathVariable Integer id) {
-        return categoryService.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Integer id) {
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Integer id) {
         categoryService.deleteByid(id);
+        return  ResponseEntity.ok().build();
     }
 }
